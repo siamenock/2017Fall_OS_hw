700bf0a0a250d409a8a5898feb9a915b3da96021
siameonck <siameonck@gmail.com>
2017-11-15 23:28:13 +0900

    hw4 done2

--- hw4_1/stop.c
+++ hw4_1/stop.c
@@ -2,7 +2,9 @@
 #include <signal.h>
 
 int sig_fn(int signo) {
+	printf("\n");
 	printf("Ctrl-C is pressed. Try Again\n");
+	printf("Ctrl-C signal number = %d\n", signo);
 }
 
 int main(int argc, char** argv) {
