a61e0bfb30ef03469b0309c2a0bee07f0bd0fc74
siamenock <siamenock@gmail.com>
2017-11-21 11:05:19 +0900

    why git push die?

--- hw5.c
+++ hw5.c
@@ -2,39 +2,74 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <time.h>
 
 
 #define MAX_Y 4000
 #define MAX_X MAX_Y
 #define MAX_THREAD_NUM 40
 
-pthread_t threads[MAX_THREAD_NUM] = {0,};
 
-typedef struct DestributionPlan {
+void GDB_Break(){}
+
+typedef struct DistributionPlan {
 	int thread_pos;
-	int destribute_to;
 	int col_start, col_end;
-} DestributionPlan;
+} DistributionPlan;
 
+DistributionPlan* MakeInitialPlan(int size, int thread_num);
+int ReturnThreadNumFromArgvs(int argc, char** argv);
+
+
+bool debug_mode = false;
+pthread_t threads[MAX_THREAD_NUM] = {0,};
+void MakeMatrixFile(char filename[]);
+void ReadMatrixFile(char filename[], int mat[MAX_Y][MAX_X]);
+void DistributeHalf(DistributionPlan* plan);
+void* ExecutePlan(void* void_plan);
 
 FILE* fp_a;
 FILE* fp_b;
-//FILE* fp_c;
 
 int A[MAX_Y][MAX_X];
 int B[MAX_Y][MAX_X];
-int C[MAX_Y][MAX_X + 1];
+long long int C[MAX_Y][MAX_X + 1];
+
+DistributionPlan* MakeInitialPlan(int size, int thread_num){
+	DistributionPlan* plan = malloc(sizeof(DistributionPlan));
+	plan->col_start 	= 0;
+	plan->col_end		= size;
+	plan->thread_pos	= thread_num -1;
+
+	threads[thread_num -1] = pthread_self();
+
+	return plan;
+}
+
+int ReturnThreadNumFromArgvs(int argc, char** argv){
+	/*
+	if (2 < argc){
+		if( strcmp(argv[2], "debug") == 0){
+			debug_mode = true;
+		}
+	}*/
+	if (1 < argc){
+		return atoi(argv[1]);
+	} else {
+		return 1;
+	}
+}
 
 void MakeMatrixFile(char filename[]) {
 	FILE* fp_temp =  fopen(filename, "w");
 	int x, y, val;
 	for (y = 0; y < MAX_Y; y++) {
 		for (x = 0; x < MAX_X; x++) {
-			val = rand() % 10;	// 0 ~ 9
-			fprintf(fp_temp, "%d ", val);
+			val = rand() % 10000;	// 0 ~ 9999
+			fprintf(fp_temp, "%6d ", val);
 		}
 		fprintf(fp_temp, "\n");
-	}
+	}	
 	fclose(fp_temp);
 };
 
@@ -46,7 +81,7 @@ void ReadMatrixFile(char filename[], int mat[MAX_Y][MAX_X]) {
 		fp_temp = fopen(filename, "r");
 	}
 	if (fp_temp == NULL) {
-		perror("ReadMatrixFile(%s) failed!\n", filename);
+		perror("ReadMatrixFile() failed!\n");
 		exit(1);
 	}
 
@@ -59,56 +94,104 @@ void ReadMatrixFile(char filename[], int mat[MAX_Y][MAX_X]) {
 
 }
 
-void DestributeHalf(DestributionPlan plan) {
-	int cols = plan.col_start - plan.col_end;
-	DestributionPlan qlan = plan;		//qlan is new plan for new thread
-	qlan.col_end		-= cols / 2;
-	plan.col_start		+= cols / 2;
-	qlan.destribute_to	/= 2;
-	plan.destribute_to	-= qlan.destribute_to;
-	qlan.thread_pos		+= plan.destribute_to;
+
+/*
+void DistributeHalf(DistributionPlan* plan) {
+	int width = plan->col_start - plan->col_end;
 	
-	pthread_create();
+
+	DistributionPlan * qlan = (DistributionPlan*) malloc(sizeof(DistributionPlan));
+	*qlan =  *plan;		//qlan is new plan for new thread
+	plan->col_start		+= width / 2;
+	qlan->col_end		=  plan->col_start;
+	qlan->distribute_to	/= 2;
+	plan->distribute_to	-= qlan->distribute_to;
+	qlan->thread_pos	-= plan->thread_pos - plan->distribute_to;
 	
+	pthread_create(&(threads[qlan->thread_pos]), NULL, ExecutePlan, (void*) qlan);	
 }
+*/
+void* ExecutePlan(void* void_plan) {
+	DistributionPlan * plan = (DistributionPlan*) void_plan;
 
-void* Plan2Thread(DestributionPlan* plan) {
-	threads[(int)plan->thread_pos] = pthread_self();
-
-	while (plan->destribute_to == 1 ) {	//
-		DestrbuteHalf(plan);
-	}
+	//if(debug_mode){printf("start thread%2d : %5d~%5d\n", plan->thread_pos, plan->col_start, plan->col_end);}
 	int x, y, i;
 	for (y = plan->col_start; y < plan->col_end; y++) {
 		C[y][MAX_X] = 0;			//row sum
 		for (x = 0; x < MAX_X; x++) {
-			C[y][x] = 0;			
+			C[y][x] = 0;
 			for (i = 0; i < MAX_X; i++) {
 				C[y][x] += A[y][i] * B[i][x];
 			}						// C[y][x] calc fin
 			C[y][MAX_X] += C[y][x];	
 		}							// row sum calc fin
 	}								// my job fin
-
+	//free(plan);
 }
 
 int main(int argc, char** argv) {
-	
+	int thread_num = 1;
+	thread_num = ReturnThreadNumFromArgvs(argc, argv);
 	srand(0);	// fixed
+	if(debug_mode){ printf("start reading a.txt\n"); }
 	ReadMatrixFile("a.txt", A);
+	if(debug_mode){ printf("start reading b.txt\n"); }
 	ReadMatrixFile("b.txt", B);
 	
-	int thread_num = 1;
-	// set thread_num here
+	if(debug_mode){
+		printf("thread_num = %d\n", thread_num);
+		//printf("plan->col_start %d, plan->col_end %d, plan->thread_pos %d, plan->distribute_to %d\n", plan->col_start, plan->col_end, plan->thread_pos, plan->distribute_to);
+	}
 
 
-	DestributionPlan plan;
-	plan.col_start	= 0;
-	plan.col_end = MAX_Y;
-	plan.destribute_to = thread_num;
-	plan.thread_pos = 0;
+	DistributionPlan plans[MAX_THREAD_NUM];
+	
+	//MAIN PART. start TIMER here
+	time_t start, end;
+	time(&start);
 
-	Plan2Thread(&plan);
+	int i, gap;
+	gap = MAX_Y / thread_num;
+	if(debug_mode) { printf("gap == %d\n",gap);}
+	for(i=0; i < thread_num -1; i++){
+		plans[i].col_start	= gap * i;
+		plans[i].col_end	= gap * (i+1);
+		plans[i].thread_pos	= i;
+		//if(debug_mode) {printf("plan->col_start %d, plan->col_end %d, plan->thread_pos %d, plan->distribute_to %d\n", plans[i].col_start, plans[i].col_end, plans[i].thread_pos, plans[i].distribute_to);}
+		pthread_create(&threads[i], NULL, ExecutePlan, &plans[i]);
+	}
+	plans[i].col_start	= gap * i;
+	plans[i].col_end	= MAX_Y;	
+	plans[i].thread_pos	= i;
 
-	return 1;
+	//if(debug_mode) {printf("plan->col_start %d, plan->col_end %d, plan->thread_pos %d, plan->distribute_to %d\n", plans[i].col_start, plans[i].col_end, plans[i].thread_pos, plans[i].distribute_to);}
+	pthread_create(&threads[i], NULL, ExecutePlan, &plans[i]);
+
+	//TODO: check end
+	long long int  sum = 0, j;
+	for (i = 0; i < thread_num; i++){
+		void* ret;
+		pthread_join(threads[i], &ret);
+		
+		for(j = plans[i].col_start; j < plans[i].col_end; j++){
+			//if(debug_mode) {printf("thread[%3d] joined, rowsum = %d\n", i, C[j][MAX_X]);}
+			sum += C[j][MAX_X];
+		}
+		//if(debug_mode) {printf("thread[%3d] joined, current sum = %10lld\n", i, sum);}
+	}
+	time(&end);
+	double time_cost = difftime(end, start);
+	printf("thread_num ==%2d\t time_cost == %fsec\t", thread_num, time_cost);
+	printf("total sum == %lld\n", sum);
+
+	return 0;
 }
+
+
+
+
+
+
+
+
+
